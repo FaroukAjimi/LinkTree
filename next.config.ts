24a0082a001b1b1fa6f844eb5e84a65234e9a55e
8AYAMT <benajimi@gmail.com>
2026-08-19 12:03:00 +0100

    Config

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     unoptimized: true, // GitHub Pages doesn't support the default Next.js Image Optimization API
   },
 
-  basePath: '/your-repository-name',
+  basePath: '/linktree',
 };
 
 export default nextConfig;
